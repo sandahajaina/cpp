57663d92c92642502fea6b3f2b0aba255b640490
Sanda Hajaina Ranaivoarisoa <hajainasanda@gmail.com>
2025-01-09 17:59:24 +0300

    module_02

---
+++ module_02/ex00/Fixed.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sranaivo <sranaivo@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 17:20:05 by sranaivo          #+#    #+#             */
+/*   Updated: 2025/01/09 17:29:27 by sranaivo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+    private:
+
+    public:
+        Fixed(/* args */);
+        ~Fixed();
+};
+
+Fixed::Fixed(/* args */)
+{
+}
+
+Fixed::~Fixed()
+{
+}
+
+
+#endif
