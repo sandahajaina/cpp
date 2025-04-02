3c80f9270b297350475a387db2e3d1e527be2862
Sanda Hajaina Ranaivoarisoa <hajainasanda@gmail.com>
2025-04-02 16:38:24 +0300

    cpp05 done

--- module_05/ex03/src/main.cpp
+++ module_05/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sranaivo <sranaivo@student.42antananarivo. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 18:22:23 by sranaivo          #+#    #+#             */
-/*   Updated: 2025/04/01 16:02:20 by sranaivo         ###   ########.fr       */
+/*   Updated: 2025/04/02 16:35:59 by sranaivo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,25 @@
 
 int main()
 {
-    Intern saotra;
+    {
+        Intern saotra;
 
-    AForm* form = saotra.makeForm("robotomy request", "Bender");
-    
-    std::cout << form->getName() << '\n';
+        AForm* form = saotra.makeForm("robotomy request", "Bender");
+        std::cout << "form: " << form->getName() << ", target: " << form->getTarget() << '\n';
 
-    delete form;
+        AForm* form1 = saotra.makeForm("presidential pardon", "John");
+        std::cout << "form: " << form1->getName() << ", target: " << form1->getTarget() << '\n';
+
+        AForm* form2 = saotra.makeForm("shrubbery creation", "home");
+        std::cout << "form: " << form2->getName() << ", target: " << form2->getTarget() << '\n';
+
+        AForm* form3 = saotra.makeForm("friend request", "Agata");
+        if (form3 == NULL)
+            std::cout << "NULL" << '\n';
+
+        delete form;
+        delete form1;
+        delete form2;
+    }
     return  0;
 }
