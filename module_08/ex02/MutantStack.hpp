97267b7373bb4c53ff3f3c1589153d35fed7f74a
Sanda Hajaina Ranaivoarisoa <hajainasanda@gmail.com>
2025-05-16 17:01:48 +0300

    cpp8 done

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: sranaivo <sranaivo@student.42antananarivo. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 14:11:46 by sranaivo          #+#    #+#             */
-/*   Updated: 2025/05/15 16:52:22 by sranaivo         ###   ########.fr       */
+/*   Updated: 2025/05/16 15:59:20 by sranaivo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,21 @@ class MutantStack : public std::stack<T>
         {
             std::stack<T>::operator=(other);
             return *this;
-        }   
+        }
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+        iterator begin() {return (this->c.begin());}
+        iterator end() {return (this->c.end());}
+        const_iterator begin() const {return (this->c.end());}
+        const_iterator end() const {return (this->c.end());}
+        reverse_iterator rbegin() {return (this->c.rbegin());}
+        reverse_iterator rend() {return (this->c.rend());}
+        const_reverse_iterator rbegin() const {return (this->c.rbegin());}
+        const_reverse_iterator rend() const {return (this->c.rend());}
 
     private:
         /* data */
--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -6,13 +6,96 @@
 /*   By: sranaivo <sranaivo@student.42antananarivo. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 14:10:59 by sranaivo          #+#    #+#             */
-/*   Updated: 2025/05/14 14:11:17 by sranaivo         ###   ########.fr       */
+/*   Updated: 2025/05/16 16:58:25 by sranaivo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutantStack.hpp"
+#include <iostream>
+#include <list>
+
 int main()
 {
-    
+    {
+        MutantStack<int> mstack;
+        mstack.push(5);
+        mstack.push(17);
+        std::cout << mstack.top() << std::endl;
+        mstack.pop();
+        std::cout << mstack.size() << std::endl;
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(737);
+        //[...]
+        mstack.push(0);
+        MutantStack<int>::iterator it = mstack.begin();
+        MutantStack<int>::iterator ite = mstack.end();
+        ++it;
+        --it;
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        std::stack<int> s(mstack);
+    }
+    {
+        std::cout << "~~~\n";
+        std::list<int> mstack;
+        mstack.push_back(5);
+        mstack.push_back(17);
+        std::cout << mstack.back() << std::endl;
+        mstack.pop_back();
+        std::cout << mstack.size() << std::endl;
+        mstack.push_back(3);
+        mstack.push_back(5);
+        mstack.push_back(737);
+        //[...]
+        mstack.push_back(0);
+        std::list<int>::iterator it = mstack.begin();
+        std::list<int>::iterator ite = mstack.end();
+        ++it;
+        --it;
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        std::list<int> s(mstack);
+    }
+    {
+        std::cout << "~~~\n";
+
+        MutantStack<std::string> mt;
+        mt.push("salut");
+        mt.push("les");
+        mt.push("gens");
+
+        MutantStack<std::string>::const_iterator it = mt.begin();
+        MutantStack<std::string>::const_iterator ite = mt.end();
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+    }
+
+    {
+        std::cout << "~~~\n";
+
+        MutantStack<std::string> mt;
+        mt.push("salut");
+        mt.push("les");
+        mt.push("gens");
+
+        MutantStack<std::string>::reverse_iterator it = mt.rbegin();
+        MutantStack<std::string>::reverse_iterator ite = mt.rend();
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+    }
 
     return 0;
 }
