b83e3e3ba56aa5542c6ae2061c22144cb0cb07b2
Sanda Hajaina Ranaivoarisoa <hajainasanda@gmail.com>
2025-06-20 14:18:58 +0300

    PmergeMe

---
+++ module_09/ex02/PmergeMe.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sranaivo <sranaivo@student.42antananarivo. +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/20 07:39:07 by sranaivo          #+#    #+#             */
+/*   Updated: 2025/06/20 13:37:16 by sranaivo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class PmergeMe
+{
+    private:
+        static void parseInput(int);
+    public:
+        static void process(int , char**);
+};
