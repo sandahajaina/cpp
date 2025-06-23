bb052a6836aa755b2d3a2af85c30be7e59afc77b
Sanda Hajaina Ranaivoarisoa <hajainasanda@gmail.com>
2025-06-23 17:21:54 +0300

    nthng

--- module_09/ex02/PmergeMe.hpp
+++ module_09/ex02/PmergeMe.hpp
@@ -6,14 +6,18 @@
 /*   By: sranaivo <sranaivo@student.42antananarivo. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 07:39:07 by sranaivo          #+#    #+#             */
-/*   Updated: 2025/06/20 13:37:16 by sranaivo         ###   ########.fr       */
+/*   Updated: 2025/06/23 17:21:03 by sranaivo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+#include <vector>
+#include <deque>
 
 class PmergeMe
 {
     private:
-        static void parseInput(int);
+        static void parseInput(int, char**, std::vector<unsigned int>&, std::deque<unsigned int>&);
     public:
         static void process(int , char**);
 };
